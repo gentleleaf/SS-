90317c7dd408a6f577365ca29b6a12336e29c3dd
gentleleaf <luhaorui.92happy@gmail.com>
2024-11-02 15:52:11 +0800

    Update ss.py

--- ss.py
+++ ss.py
@@ -32,10 +32,22 @@ if j.status_code == 200:
     l = binascii.unhexlify(k)
     m = f(l, d, e)
     n = json.loads(m)
-    with open(output_file_path, 'w') as file: 
+
+    # 获取当前时间并格式化为字符串
+    current_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    
+    # 创建一个时间节点
+    time_node = f"aes-256-cfb:time_node@0.0.0.0:0"  # 使用占位符的节点信息
+    time_node_base64 = base64.b64encode(time_node.encode('utf-8')).decode('utf-8')
+    time_subscribe = f"ss://{time_node_base64}#{current_time}"
+    
+    with open(output_file_path, 'w') as file:  # 使用 'w' 模式覆盖写入文件
+        # 写入时间节点到文件
+        file.write(time_subscribe + '\n')
+    
         for o in n['data']:
             p = f"aes-256-cfb:{o['password']}@{o['ip']}:{o['port']}"
             q = base64.b64encode(p.encode('utf-8')).decode('utf-8')
             r = f"ss://{q}#{o['title']}"
             print(r)
-            file.write(r + '\n')  
+            file.write(r + '\n')  # 将输出写入文件
